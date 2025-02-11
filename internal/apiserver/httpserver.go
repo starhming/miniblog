1cc6dff657ee2e498b45a48abfd40ea0a4fec5bc
孔令飞 <colin404@foxmail.com>
2025-02-11 22:44:30 +0800

    refactor: 优化代码结构
    Change-Id: I5d46d074781f2d2d5d5c0d7e6dff590d203956d7

---
+++ internal/apiserver/httpserver.go
@@ -0,0 +1,32 @@
+// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/onexstack/miniblog. The professional
+// version of this repository is https://github.com/onexstack/onex.
+
+package apiserver
+
+import (
+	"context"
+
+	"github.com/onexstack/miniblog/internal/pkg/server"
+)
+
+// ginServer 定义一个使用 Gin 框架开发的 HTTP 服务器.
+type ginServer struct{}
+
+// 确保 *ginServer 实现了 server.Server 接口.
+var _ server.Server = (*ginServer)(nil)
+
+// NewGinServer 初始化一个新的 Gin 服务器实例.
+func (c *ServerConfig) NewGinServer() server.Server {
+	return &ginServer{}
+}
+
+// RunOrDie 启动 Gin 服务器，出错则程序崩溃退出.
+func (s *ginServer) RunOrDie() {
+	select {}
+}
+
+// GracefulStop 优雅停止服务器.
+func (s *ginServer) GracefulStop(ctx context.Context) {}
---
+++ internal/pkg/server/http_server.go
@@ -0,0 +1,48 @@
+// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/onexstack/miniblog. The professional
+// version of this repository is https://github.com/onexstack/onex.
+
+package server
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	genericoptions "github.com/onexstack/onexstack/pkg/options"
+
+	"github.com/onexstack/miniblog/internal/pkg/log"
+)
+
+// HTTPServer 代表一个 HTTP 服务器.
+type HTTPServer struct {
+	srv *http.Server
+}
+
+// NewHTTPServer 创建一个新的 HTTP 服务器实例.
+func NewHTTPServer(httpOptions *genericoptions.HTTPOptions, handler http.Handler) *HTTPServer {
+	return &HTTPServer{
+		srv: &http.Server{
+			Addr:    httpOptions.Addr,
+			Handler: handler,
+		},
+	}
+}
+
+// RunOrDie 启动 HTTP 服务器并在出错时记录致命错误.
+func (s *HTTPServer) RunOrDie() {
+	log.Infow("Start to listening the incoming requests", "protocol", protocolName(s.srv), "addr", s.srv.Addr)
+	if err := s.srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalw("Failed to server HTTP(s) server", "err", err)
+	}
+}
+
+// GracefulStop 优雅地关闭 HTTP 服务器.
+func (s *HTTPServer) GracefulStop(ctx context.Context) {
+	log.Infow("Gracefully stop HTTP(s) server")
+	if err := s.srv.Shutdown(ctx); err != nil {
+		log.Errorw("HTTP(s) server forced to shutdown", "err", err)
+	}
+}
