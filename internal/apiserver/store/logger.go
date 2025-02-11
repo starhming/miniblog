47b428a839c93f2d3360cdcc84e5bbbfcab4737f
孔令飞 <colin404@foxmail.com>
2025-02-11 22:45:31 +0800

    feat: 实现简洁架构的Store层
    Change-Id: I3d5080d08b84c88afd322900ca885f70a3455294

---
+++ internal/apiserver/store/logger.go
@@ -0,0 +1,25 @@
+// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/onexstack/miniblog. The professional
+// version of this repository is https://github.com/onexstack/onex.
+
+package store
+
+import (
+	"github.com/onexstack/miniblog/internal/pkg/log"
+)
+
+// Logger is a logger that implements the Logger interface.
+// It uses the log package to log error messages with additional context.
+type Logger struct{}
+
+// NewLogger creates and returns a new instance of Logger.
+func NewLogger() *Logger {
+	return &Logger{}
+}
+
+// Error logs an error message with the provided context using the log package.
+func (l *Logger) Error(err error, msg string, kvs ...any) {
+	log.Errorw(msg, append(kvs, "err", err)...)
+}
